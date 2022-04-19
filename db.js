38fcf848970618c1306d33a0416ecfd3b508a957
ghost-vk <ghost.vkv@gmail.com>
2022-04-19 20:48:35 +0300

    Update db settings.

--- db.js
+++ db.js
@@ -3,12 +3,9 @@ const Pool = require('pg').Pool
 const debug = require('debug')('db')
 require('dotenv').config({ path: path.resolve(__dirname, '/.env') })
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = !isProd
-
-const user = isDev ? process.env.LOCAL_DB_USER : process.env.REMOTE_DB_USER
-const database = isDev ? process.env.LOCAL_DB_NAME : process.env.REMOTE_DB_NAME
-const password = isDev ? process.env.LOCAL_DB_PASSWORD : process.env.REMOTE_DB_PASSWORD
+const user = process.env.DB_USER
+const database = process.env.DB_NAME
+const password = process.env.DB_PASSWORD
 
 debug('Connect to Database: %s', database)
 debug('User: %s', user)
